0be38772ee4097ab043d39fbea244ba861548660
Andrew Mason <andrew.mason@n2pub.com>
2019-01-26 09:42:46 -0500

    few minor style tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,13 @@ export default class IndexPage extends React.Component {
         <section className="w-full min-h-screen">
           <div className="min-h-screen w-full sm:w-3/4 md:w-1/2 mx-auto container flex flex-col items-center justify-center">
             <div className="text-center md:text-center pt-12 px-8">
-              <h1 className="text-4xl text-blue mt-4 md:mt-12 mb-4 md:mb-8 text-center">
+              <h1 className="text-4xl text-blue mt-8 md:mt-12 mb-4 md:mb-8 text-center">
                 {' '}
                 My Photos{' '}
               </h1>
             </div>
             {posts.map(({ node: post }) => (
-              <div className="" key={post.id}>
+              <div className="px-8" key={post.id}>
                 <p>
                   <Link className="" to={post.fields.slug}>
                     {post.frontmatter.title}
